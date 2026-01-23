d9216e4edfded7426cf7b3fcad83bb064bf4d87a
unknown <braynyuwono@gmail.com>
2026-01-23 08:45:25 +0700

    perbaikan v1.6.8.0

--- routes/web.php
+++ routes/web.php
@@ -8,14 +8,14 @@ use App\Http\Controllers\NearMissController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ImportController;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return redirect()->route('login');
+});
 
 // ========== AUTHENTICATION ROUTES ==========
 // Guest routes (only accessible when NOT logged in)
 Route::middleware('guest')->group(function () {
-    Route::get('/', [AuthController::class, 'showLogin'])->name('login');
+    Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
     Route::post('/login', [AuthController::class, 'login'])->name('login.store');
     Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
     Route::post('/register', [AuthController::class, 'register'])->name('register.store');
