bd9749f0bfa8d91c8ff405e857a0284233b5f326
unknown <braynyuwono@gmail.com>
2026-01-28 08:40:34 +0700

    perbaikan v1.7.3.8

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -68,7 +68,7 @@
                                         class="inline-flex items-center gap-2 px-4 py-2 text-sm font-medium
                                                text-white bg-blue-600 rounded-lg hover:bg-blue-700
                                                focus:outline-none focus:ring-2 focus:ring-blue-400">
-                                        ✔ Approve
+                                        Approve
                                     </button>
                                 </form>
                             </td>
