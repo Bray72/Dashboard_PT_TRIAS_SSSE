ce25c470f41d0c0beaaa64cd3ffd85b6e0bc306b
unknown <braynyuwono@gmail.com>
2026-01-29 14:53:47 +0700

    perbaikan v1.7.7.4

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -111,11 +111,18 @@
                                 </span>
                             </div>
                         </button>
-                        <div class="absolute left-0 mt-0 w-48 bg-white rounded-md shadow-lg hidden group-hover:block z-50 dark:bg-gray-800">
-                            <a href="{{ route('admin.users') }}" class="block px-4 py-2 text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-700">User Approval</a>
-                        </div>
-                        <div class="absolute left-0 mt-0 w-48 bg-white rounded-md shadow-lg hidden group-hover:block z-50 dark:bg-gray-800">
-                            <a href="{{ route('activity') }}" class="block px-4 py-2 text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-700">Last Activity</a>
+                        <div class="absolute left-0 mt-2 w-48 bg-white rounded-md shadow-lg hidden group-hover:block z-50 dark:bg-gray-800">
+                            <a href="{{ route('admin.users') }}"
+                            class="block px-4 py-2 text-gray-700 hover:bg-gray-100
+                                    dark:text-gray-300 dark:hover:bg-gray-700">
+                                User Approval
+                            </a>
+
+                            <a href="{{ route('activity') }}"
+                            class="block px-4 py-2 text-gray-700 hover:bg-gray-100
+                                    dark:text-gray-300 dark:hover:bg-gray-700">
+                                Last Activity
+                            </a>
                         </div>
                     </div>
 
