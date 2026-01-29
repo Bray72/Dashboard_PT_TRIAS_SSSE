f0254ac24f65b9c6a9dd4d98d98d3fc6e0f06dce
unknown <braynyuwono@gmail.com>
2026-01-29 14:11:06 +0700

    perbaikan v1.7.7.0

--- app/Http/Controllers/Admin/ActivityController.php
+++ app/Http/Controllers/Admin/ActivityController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Models\NearMiss;
+use App\Models\SafetyMetric;
+use App\Models\WorkPermit;
 use Illuminate\Http\Request;
 use Illuminate\Support\Collection;
 
@@ -18,25 +20,55 @@ class ActivityController extends Controller
          * 1. USER REGISTER
          */
         User::latest('created_at')
-            ->take(5)
+            ->take(10)
             ->get()
             ->each(function ($user) use ($activities) {
                 $activities->push([
                     'message' => "{$user->name} register",
                     'time' => $user->created_at,
+                    'type' => 'register',
                 ]);
             });
 
         /**
-         * 2. USER INPUT DATA (contoh: Near Miss)
+         * 2. SAFETY METRIC INPUT
+         */
+        SafetyMetric::latest('created_at')
+            ->take(10)
+            ->get()
+            ->each(function ($item) use ($activities) {
+                $activities->push([
+                    'message' => "Input data Safety Metric",
+                    'time' => $item->created_at,
+                    'type' => 'safety_metric',
+                ]);
+            });
+
+        /**
+         * 3. WORK PERMIT INPUT
+         */
+        WorkPermit::latest('created_at')
+            ->take(10)
+            ->get()
+            ->each(function ($item) use ($activities) {
+                $activities->push([
+                    'message' => "Input data Work Permit",
+                    'time' => $item->created_at,
+                    'type' => 'work_permit',
+                ]);
+            });
+
+        /**
+         * 4. NEAR MISS INPUT
          */
         NearMiss::latest('created_at')
-            ->take(5)
+            ->take(10)
             ->get()
             ->each(function ($item) use ($activities) {
                 $activities->push([
                     'message' => "Input data Near Miss",
                     'time' => $item->created_at,
+                    'type' => 'near_miss',
                 ]);
             });
 
@@ -45,7 +77,7 @@ class ActivityController extends Controller
          */
         $activities = $activities
             ->sortByDesc('time')
-            ->take(10);
+            ->take(15);
 
         return view('dashboard.activity', compact('activities'));
     }
